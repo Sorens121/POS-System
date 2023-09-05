d6e8614d3a90c573923bfd51bb750836dd0f73cc
Jagamohan Kumar Soren <sorens121@gmail.com>
2023-09-05 23:49:36 +0530

    added a delay to launch the application

--- src/main/java/com/pos/inventorysystem/Main.java
+++ src/main/java/com/pos/inventorysystem/Main.java
@@ -1,20 +1,48 @@
 package com.pos.inventorysystem;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.util.Objects;
+
 public class Main extends Application {
+
+    public static boolean configFileExists() {
+        String configFilePath = "C:/Users/TROJAN HORSE/Java Project/POS Inventory System/src/main/resources/config.properties";;
+        File configFile = new File(configFilePath);
+
+        return configFile.exists();
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
-        //Parent root = FXMLLoader.load(getClass().getResource("AppExampleMain.fxml"));
+        //check if the config file exists
+        if(!configFileExists()){
+            Platform.runLater(() -> showErrorDialog("Configuration file not found.", "Please make sure that config file is present."));
+            return;
+        }
+
+        // Initializing and starting the javafx application here
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("home.fxml")));
         Scene scene = new Scene(root, 1200, 800);
         stage.setTitle("POS System");
         stage.setScene(scene);
         stage.show();
+    }
+
+    // TODO: 05-09-2023  Need to create a splash screen while the application searches for config.properties file
+    private void showErrorDialog(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
     public static void main(String[] args) {
