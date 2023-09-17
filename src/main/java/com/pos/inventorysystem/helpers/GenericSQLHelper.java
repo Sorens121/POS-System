c6063335820f8f0b34e13db01b8e1bdb6c6fd4a7
Jagamohan Kumar Soren <sorens121@gmail.com>
2023-09-17 12:50:58 +0530

    added new method

--- src/main/java/com/pos/inventorysystem/helpers/GenericSQLHelper.java
+++ src/main/java/com/pos/inventorysystem/helpers/GenericSQLHelper.java
@@ -5,6 +5,7 @@ import com.pos.inventorysystem.db.db;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class GenericSQLHelper {
 
@@ -13,14 +14,43 @@ public class GenericSQLHelper {
             preparedStatement.setString(1, valueToCheck);
             ResultSet resultSet = preparedStatement.executeQuery();
             if(resultSet.next()){
-                return true;
-            } else {
                 return false;
+            } else {
+                return true;
             }
         } catch (SQLException | ClassNotFoundException e){
             e.printStackTrace();
             System.out.println("Error while running the query");
         }
+        return true;
+    }
+
+    public static boolean shouldUpdateRow(String selectQuery, String id, List<String> params) {
+        try(PreparedStatement preparedStatement = db.myConnection().prepareStatement(selectQuery)) {
+            preparedStatement.setString(1, id);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+
+                if (resultSet.next()) {
+                    //Iterate through the params and compare with the ResultSet values
+                    for (int i = 1; i <= params.size(); i++) {
+                        String currentValue = resultSet.getString(i+1);
+                        String newValue = params.get(i-1);
+                        System.out.println("current values :" + currentValue);
+                        System.out.println("new values :" + newValue);
+                        //compare current value with the params value
+                        if (!newValue.equals(currentValue)) {
+                            return true; // change detected hence update is required
+                        }
+                    }
+                    return false;
+                }
+
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Error while running the query");
+        }
+
         return false;
     }
 }
