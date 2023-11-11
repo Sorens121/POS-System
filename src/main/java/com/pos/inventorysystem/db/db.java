24c161fbf690d4c547ab9f6a76757b2d38ba2f34
Jagamohan Kumar Soren <sorens121@gmail.com>
2023-11-11 07:03:48 +0530

    modified the code to release resources after query is completed

--- src/main/java/com/pos/inventorysystem/db/db.java
+++ src/main/java/com/pos/inventorysystem/db/db.java
@@ -2,23 +2,59 @@ package com.pos.inventorysystem.db;
 
 import com.pos.inventorysystem.utils.ConfigFileManager;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class db {
-    public static Connection myConnection() throws SQLException, ClassNotFoundException {
+    public static Connection establishConnection() throws SQLException {
         ConfigFileManager configFileManager = new ConfigFileManager();
+        Connection connection = null;
+
         try{
             String url = configFileManager.getProperty("database.url");
             String user = configFileManager.getProperty("database.username");
             String password = configFileManager.getProperty("database.password");
 
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(url, user, password);
-        } catch(SQLException | ClassNotFoundException e) {
-            System.out.println(e.getMessage());
-            throw e;
+            connection = DriverManager.getConnection(url, user, password);
+            connection.setAutoCommit(false);
+        } catch(SQLException e) {
+            System.out.println("Connection error " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
+    public static void closeConnection(Connection connection, Statement statement, PreparedStatement preparedStatement, ResultSet resultSet) {
+        if(resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(preparedStatement != null) {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
